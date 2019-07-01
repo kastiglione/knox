561549f08d9bac76fdd730afa24d70e696b08f78
Dave Lee <davelee.com@gmail.com>
2019-07-01 11:08:22 -0600

    Fix auditon -h/--help strcmp

--- auditon.cpp
+++ auditon.cpp
@@ -42,7 +42,7 @@ int main(int argc, char **argv) {
     return usage();
   }
 
-  if (strcmp(argv[1], "-h") || strcmp(argv[1], "--help")) {
+  if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
     return usage();
   }
 
